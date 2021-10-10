af736d74ed6dc019e7bd9f093b9d78b26ea90dc6
c0053303 <t.goulandris1@newcastle.ac.uk>
2021-10-10 16:08:03 +0100

    Added Validation error and made a function so that special characters weren't allowed to be inputted into the users first and last name

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import Required, Email, Length, EqualTo
+from wtforms.validators import Required, Email, Length, EqualTo, ValidationError
+
+
+def special_characters_search(form, field):
+    exclude_special_characters = "*?!'^%&/()=}][{$#@<>"
+    for characters in field.data:
+        if characters in exclude_special_characters:
+            raise ValidationError(f"Special characters {characters} are not allowed.")
 
 
 class RegisterForm(FlaskForm):
     email = StringField(validators=[Required(), Email()])
-    firstname = StringField(validators=[Required()])
-    lastname = StringField(validators=[Required()])
+    firstname = StringField(validators=[Required(), special_characters_search])
+    lastname = StringField(validators=[Required(), special_characters_search])
     phone = StringField(validators=[Required()])
     password = PasswordField(validators=[Required(), Length(min=6, max=12, message='Password needs to have a minimum '
                                                                                    'length of 6 characters or a '
