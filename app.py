d2ba5f19d242ebd57928be961bc4bca1dea392a0
c0053303 <t.goulandris1@newcastle.ac.uk>
2021-10-11 15:14:52 +0100

    Added Error handling 404,403 and 500 view functions

--- app.py
+++ app.py
@@ -12,6 +12,19 @@ app.config['SECRET_KEY'] = 'LongAndRandomSecretKey'
 # initialise database
 db = SQLAlchemy(app)
 
+# Error Page
+@app.errorhandler(403)
+def page_forbidden(error):
+    return render_template('403.html'), 403
+
+@app.errorhandler(404)
+def page_not_found(error):
+     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
 
 # HOME PAGE VIEW
 @app.route('/')
