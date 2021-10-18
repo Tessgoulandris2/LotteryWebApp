d75be51a1d25a508796286fbbb71b61f258bc718
c0053303 <t.goulandris1@newcastle.ac.uk>
2021-10-18 16:38:13 +0100

    Created a function that checks that the user is inputting their phone number in the correct format

--- users/forms.py
+++ users/forms.py
@@ -11,21 +11,29 @@ def special_characters_search(form, field):
             raise ValidationError(f"Special characters {characters} are not allowed.")
 
 
+def password_authentication(self, password):
+    p = re.compile(r'(?=.*\d)(?=.*[a-zA-Z])(?=.*[0-9][1-9])')
+    if not p.match(self.password.data):
+        raise ValidationError("Password must contain 1 digit and 1 upper and lowercase letter and 1 special "
+                              "character.")
+
+
+def phone_format(self, field):
+    p = re.compile(r'\d{4}-\d{3}\d{4}')
+    if not p.match(field.data):
+        raise ValidationError(f"Please enter your phone number in the format XXXX-XXX-XXXX.")
+
+
 class RegisterForm(FlaskForm):
     email = StringField(validators=[Required(), Email()])
     firstname = StringField(validators=[Required(), special_characters_search])
     lastname = StringField(validators=[Required(), special_characters_search])
-    phone = StringField(validators=[Required()])
+    phone = StringField(validators=[Required(), phone_format])
     password = PasswordField(validators=[Required(), Length(min=6, max=12, message='Password needs to have a minimum '
                                                                                    'length of 6 characters or a '
-                                                                                   'maximum of 12.')])
+                                                                                   'maximum of 12.'),
+                                         password_authentication])
     confirm_password = PasswordField(validators=[Required(), EqualTo('password', message='Both passwords inputs must '
                                                                                          'match.')])
-    pin_key = StringField(validators=[Required()])
+    pin_key = StringField(validators=[Required(), Length(32, message='Pin Key must be 32 characters long')])
     submit = SubmitField()
-
-    def password_authentication(self, password):
-        p = re.compile(r'(?=.*\d)(?=.*[a-zA-Z])(?=.*[0-9][1-9])')
-        if not p.match(self.password.data):
-            raise ValidationError("Password must contain at 1 digit and 1 upper and lowercase letter and 1 special "
-                                  "character.")
