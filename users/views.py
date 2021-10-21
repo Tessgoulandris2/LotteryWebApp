e5e570b22c470c1496178a6495a19ef08443a9ca
c0053303 <t.goulandris1@newcastle.ac.uk>
2021-10-21 14:59:26 +0100

    Adding Logout.html to the url

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 # IMPORTS
 from flask import Blueprint, render_template, flash, redirect, url_for, request
-from flask_login import login_user
+from flask_login import login_user, logout_user
 from werkzeug.security import check_password_hash
 from app import db
 from models import User
